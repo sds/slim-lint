c481495d793b750f504234654fcedd05d8895f9b
Shane da Silva <shane@dasilva.io>
2015-04-25 14:06:50 -0700

    Add spec for SexpConverter filter

---
+++ spec/slim_lint/filters/sexp_converter_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe SlimLint::Filters::SexpConverter do
+  describe '#call' do
+    let(:array) { [:one, :two, [:three_one, :three_two]] }
+    subject { described_class.new.call(array) }
+
+    it 'returns a Sexp' do
+      # The Sexp#initialize spec tests the actual conversion
+      subject.should be_a SlimLint::Sexp
+    end
+  end
+end

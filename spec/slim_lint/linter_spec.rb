26d620ea57b8c226e7bfdb2d5598ba4f328d3784
Shane da Silva <shane@dasilva.io>
2015-04-25 17:17:52 -0700

    Test parse_ruby helper in Linter

--- spec/slim_lint/linter_spec.rb
+++ spec/slim_lint/linter_spec.rb
@@ -13,15 +13,32 @@ describe SlimLint::Linter do
   let(:linter) { linter_class.new(config) }
 
   describe '#run' do
-    let(:document) do
-      double(sexp: SlimLint::Sexp.new([:one, [:lint], :two, [:lint]]),
-             file: 'file.slim')
-    end
-
+    let(:sexp) { [:one, [:lint], :two, [:lint]] }
+    let(:document) { double(sexp: SlimLint::Sexp.new(sexp), file: 'file.slim') }
     subject { linter.run(document) }
 
     it 'returns the reported lints' do
-      subject.length == 2
+      subject.length.should == 2
+    end
+
+    context 'when a linter calls parse_ruby' do
+      let(:linter_class) do
+        Class.new(described_class) do
+          attr_reader :parsed_ruby
+
+          on [:ruby] do |sexp|
+            _, ruby = sexp
+            @parsed_ruby = parse_ruby(ruby)
+          end
+        end
+      end
+
+      let(:sexp) { [:ruby, "puts 'Hello world'"] }
+
+      it 'parses the ruby' do
+        subject
+        linter.parsed_ruby.type.should == :send
+      end
     end
   end
 

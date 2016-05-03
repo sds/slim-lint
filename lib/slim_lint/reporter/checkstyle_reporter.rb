8c30119c743f65a0735c9cd167c58f2fb8687eb6
Christof Damian <christof@damian.net>
2016-05-03 17:46:56 +0200

    some cleanup and tests

--- lib/slim_lint/reporter/checkstyle_reporter.rb
+++ lib/slim_lint/reporter/checkstyle_reporter.rb
@@ -7,13 +7,13 @@ module SlimLint
       document = REXML::Document.new.tap do |d|
         d << REXML::XMLDecl.new
       end
-      @checkstyle = REXML::Element.new('checkstyle', document)
+      checkstyle = REXML::Element.new('checkstyle', document)
 
       report.lints.group_by(&:filename).map do |lint|
-        map_file(lint, @checkstyle)
+        map_file(lint, checkstyle)
       end
 
-      document.write
+      log.log document.to_s
     end
 
     private
@@ -31,18 +31,9 @@ module SlimLint
     def map_offense(offence, parent)
       REXML::Element.new('error', parent).tap do |e|
         e.attributes['line'] = offence.line
-        e.attributes['severity'] = to_checkstyle_severity(offence.severity.to_s)
+        e.attributes['severity'] = offence.error? ? 'error' : 'warning'
         e.attributes['message'] = offence.message
         e.attributes['source'] = 'slim-lint'
-      end
-    end
-
-    def to_checkstyle_severity(rubocop_severity)
-      case rubocop_severity.to_s
-      when 'fatal', 'error' then 'error'
-      when 'warning' then 'warning'
-      when 'convention', 'refactor' then 'info'
-      else 'warning'
       end
     end
   end
---
+++ spec/slim_lint/reporter/checkstyle_reporter_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe SlimLint::Reporter::CheckstyleReporter do
+  describe '#display_report' do
+    let(:io) { StringIO.new }
+    let(:output) { io.string }
+    let(:logger) { SlimLint::Logger.new(io) }
+    let(:report) { SlimLint::Report.new(lints, []) }
+    let(:reporter) { described_class.new(logger) }
+
+    subject { reporter.display_report(report) }
+
+    context 'when there are no lints' do
+      let(:lints) { [] }
+      let(:files) { [] }
+
+      it 'list of files is empty' do
+        subject
+        expect(output).to eq("<?xml version='1.0'?><checkstyle/>\n")
+      end
+    end
+
+    context 'when there are lints' do
+      let(:lint_data) do
+        [
+          {
+            filename: 'some-filename.slim',
+            line: 502,
+            description: 'Description of lint 1',
+            severity: :warning,
+          },
+          {
+            filename: 'other-filename.slim',
+            line: 724,
+            description: 'Description of lint 2',
+            severity: :error,
+          },
+        ]
+      end
+
+      let(:lints) do
+        lint_data.map do |data|
+          SlimLint::Lint.new(
+            nil,
+            data[:filename],
+            data[:line],
+            data[:description],
+            data[:severity]
+          )
+        end
+      end
+
+      it 'list of files contains files with offenses' do
+        subject
+        expect(output).to include("<file name='#{lint_data[0][:filename]}'>")
+        expect(output).to include("<file name='#{lint_data[1][:filename]}'>")
+        expect(output).to include(
+          "<error line='#{lint_data[0][:line]}' "\
+          "message='#{lint_data[0][:description]}' "\
+          "severity='#{lint_data[0][:severity]}' "\
+          "source='slim-lint'/>")
+      end
+    end
+  end
+end

455706c6cf23f7efb5168c2348e14971b3ab4abf
Max Melentiev <melentievm@gmail.com>
2017-06-30 13:55:52 +0300

    Linter for single blank line in the end of file

---
+++ spec/slim_lint/linter/trailing_blank_lines_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe SlimLint::Linter::TrailingBlankLines do
+  include_context 'linter'
+
+  context 'when line does not have line feed' do
+    let(:slim) { '.style' }
+
+    it { should report_lint line: 1 }
+  end
+
+  context 'when last line does not have line feed' do
+    let(:slim) { ".style\n  .other" }
+
+    it { should report_lint line: 2 }
+  end
+
+  context 'when line contains multiple trailing newline' do
+    let(:slim) { ".style\n\n" }
+
+    it { should report_lint line: 2 }
+  end
+
+  context 'when line contains trailing newline' do
+    let(:slim) { ".style\n" }
+
+    it { should_not report_lint }
+  end
+
+  context 'when last line does not have line feed' do
+    let(:slim) { ".style\n  .other\n" }
+
+    it { should_not report_lint }
+  end
+end

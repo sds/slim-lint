883640a1ee4cb8e319b1a5aa27fd14669f934368
tatsuya <t2h5k0@gmail.com>
2020-12-18 20:06:11 +0900

    fix test
    disable EmbeddedEngines by default
    
    fix test

--- spec/slim_lint/linter/embedded_engines_spec.rb
+++ spec/slim_lint/linter/embedded_engines_spec.rb
@@ -5,45 +5,40 @@ require 'spec_helper'
 describe SlimLint::Linter::EmbeddedEngines do
   include_context 'linter'
 
-  context 'when a file contains forbidden embedded engine' do
-    context 'default' do
-      let(:slim) { <<-SLIM }
-        h1 heading
+  context 'default configuration' do
+    let(:slim) { <<-SLIM }
+      h1 heading
 
-        javascript:
-          alert('foo')
+      javascript:
+        alert('foo')
 
-        css:
-          h1 {
-            font-size: 10px;
-          }
-      SLIM
-
-      it { should_not report_lint }
-    end
-
-    context 'add forbidden engines' do
-      let(:config) do
-        {
-          'enabled' => true, 'include' => [], 'exclude': [],
-          'forbidden_engines' => %w[javascript css],
+      css:
+        h1 {
+          font-size: 10px;
         }
-      end
+    SLIM
 
-      let(:slim) { <<-SLIM }
-        h1 heading
+    it { should_not report_lint }
+  end
 
-        javascript:
-          alert('foo')
-
-        css:
-          h1 {
-            font-size: 10px;
-          }
-      SLIM
-
-      it { should report_lint line: 3 }
-      it { should report_lint line: 6 }
+  context 'when a file contains forbidden embedded engine' do
+    let(:config) do
+      { 'forbidden_engines' => %w[javascript css] }
     end
+
+    let(:slim) { <<-SLIM }
+      h1 heading
+
+      javascript:
+        alert('foo')
+
+      css:
+        h1 {
+          font-size: 10px;
+        }
+    SLIM
+
+    it { should report_lint line: 3 }
+    it { should report_lint line: 6 }
   end
 end

f4d868c0d805b661e53d930999356fb3cc179b00
tatsuya <t2h5k0@gmail.com>
2021-03-02 12:40:15 +0900

    Linter for embedded engines (#120)
    * add embedded_engines linter
    
    * update document
    
    * fix test
    
    disable EmbeddedEngines by default
    
    fix test
    
    * disable EmbeddedEngines by default
    
    * fix README.md

---
+++ spec/slim_lint/linter/embedded_engines_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SlimLint::Linter::EmbeddedEngines do
+  include_context 'linter'
+
+  context 'default configuration' do
+    let(:slim) { <<-SLIM }
+      h1 heading
+
+      javascript:
+        alert('foo')
+
+      css:
+        h1 {
+          font-size: 10px;
+        }
+    SLIM
+
+    it { should_not report_lint }
+  end
+
+  context 'when a file contains forbidden embedded engine' do
+    let(:config) do
+      { 'forbidden_engines' => %w[javascript css] }
+    end
+
+    let(:slim) { <<-SLIM }
+      h1 heading
+
+      javascript:
+        alert('foo')
+
+      css:
+        h1 {
+          font-size: 10px;
+        }
+    SLIM
+
+    it { should report_lint line: 3 }
+    it { should report_lint line: 6 }
+  end
+end

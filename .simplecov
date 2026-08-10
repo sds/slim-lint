995dc3c366cf0cfef4c8887d71050227dac1cccd
Braden Rich <bradenwrich@gmail.com>
2026-08-10 17:28:25 -0400

    Revert "Fix SimpleCov deprecation warnings in test suite"
    This reverts commit 3d7bb40b18a5e006502a4750de68cf0e1538d7c6.

---
+++ .simplecov
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+SimpleCov.start do
+  add_filter '/bin/'
+  add_filter '/spec/'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,6 @@
 # coverage percentage in the console.
 require 'simplecov'
 
-SimpleCov.start do
-  skip '/bin/'
-  skip '/spec/'
-end
-
 require 'slim_lint'
 require 'rspec/its'
 
